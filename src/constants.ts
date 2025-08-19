83584da3659a1930e371df7170936c657d93ab39
Kouba Jiří <jiri.kouba@generaliceska.cz>
2025-08-19 17:56:01 +0200

    fix frame count

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const SONG_LENGTH = 138 as const; // in seconds
+export const SONG_LENGTH = 138.352948 as const; // in seconds
 
 // VIDEO constants
 
@@ -7,12 +7,12 @@ export const SONG_LENGTH = 138 as const; // in seconds
  */
 export const FRAME_RATE = 12 as const;
 
-export const FRAME_COUNT = FRAME_RATE * SONG_LENGTH;
+export const FRAME_COUNT = Math.floor(FRAME_RATE * SONG_LENGTH);
 
 export const OUTPUT_DIR = "dist/frames";
 
 // Length of each sound that have fixed length in frames
-export const KICK_LENGHT = FRAME_RATE / 6; //TODO for 24 fps shorten it to FRAME_RATE / 8
+export const KICK_LENGTH = FRAME_RATE / 6; //TODO for 24 fps shorten it to FRAME_RATE / 8
 export const SNARE_1_LENGTH = FRAME_RATE / 6;
 export const SNARE_2_LENGTH = FRAME_RATE / 2;
 
--- src/state/kick.ts
+++ src/state/kick.ts
@@ -1,9 +1,9 @@
-import { KICK_LENGHT } from "../constants.js";
+import { KICK_LENGTH } from "../constants.js";
 import { STATE } from "./state.js";
 
 export const isKickPlaying = (frame: number): boolean => {
 	return (
-		(STATE.kick.filter((event) => event.frame + KICK_LENGHT + 1 >= frame)?.[0]
+		(STATE.kick.filter((event) => event.frame + KICK_LENGTH + 1 >= frame)?.[0]
 			?.frame ?? Number.MIN_SAFE_INTEGER) <= frame
 	);
 };
